b54e3af548e7acb2c0e180b55f9adbc2baf8a59c
Colleen Noel <noel.colleen@gmail.com>
2020-04-09 13:44:23 -0400

    answer

--- orderBook.js
+++ orderBook.js
@@ -1,13 +1,47 @@
 function reconcileOrder(existingBook, incomingOrder) {
-  if (existingBook.length === 0) {
+  let orderIndex = findMatch(existingBook, incomingOrder)
+
+  if (orderIndex === -1) {
     existingBook.push(incomingOrder)
+    return existingBook
   }
-  return existingBook
+  else {
+
+    let matchingOrderArr = existingBook.splice(orderIndex, 1)
+    let matchingOrder = matchingOrderArr[0]
+
+    // I don't need this code, just wanted to show I did test for this condition
+    // if (matchingOrder.quantity === incomingOrder.quantity) {
+    // }
+
+    if (matchingOrder.quantity > incomingOrder.quantity) {
+      matchingOrder.quantity = matchingOrder.quantity - incomingOrder.quantity
+      existingBook.push(matchingOrder)
+    }
+
+    else if (matchingOrder.quantity < incomingOrder.quantity) {
+      incomingOrder.quantity = incomingOrder.quantity - matchingOrder.quantity
+      existingBook = reconcileOrder(existingBook, incomingOrder)
+    }
+
+    return existingBook
+  }
+
+
+
+
+
+
+  function findMatch(existingBook, incomingOrder) {
+    for (let i = 0; i < existingBook.length; i++) {
+      if (existingBook[i].type !== incomingOrder.type &&
+        existingBook[i].price === incomingOrder.price)
+        return i
+    }
+    return -1
+  }
+
 }
-
-//if book has type element sell in it, add incoming order
-
-
 
 
 
